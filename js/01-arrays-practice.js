71d7d674aede17c178c9aabc4033d129ff92a491
Lamar Clapham <lamar.clapham@gmail.com>
2020-03-05 18:49:57 -0800

    Problems 1-4 complated

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,12 +1,57 @@
 //STEP 1
+// window.console.log("#### Step 1 #####");
+// let myMovies = ["The Jungle", "The Big Lebowski", "11/22/63", "Employee of the Month", "Shawn of the Dead"];
+// window.console.log(myMovies[1]);
 
 //STEP 2
+// window.console.log("#### Step 2 ####");
+
+// let movies = new Array(5);
+
+// movies[0] = "The Jungle";
+// movies[1] = "The Big Lebowski";
+// movies[2] = "11/22/63";
+// movies[3] = "Employee of the Month";
+// movies[4] = "Shawn of the Dead";
+
+// window.console.log(movies[0]);
 
 //STEP 3
+// window.console.log("#### Step 3 ####");
+
+// let movies = new Array(5);
+
+// movies[0] = "The Jungle";
+// movies[1] = "The Big Lebowski";
+// movies[2] = "11/22/63";
+// movies[3] = "Employee of the Month";
+// movies[4] = "Shawn of the Dead";
+// movies[5] = "Blade Runner";
+
+// window.console.log(movies[0]);
+// window.console.log("The array lenght is : "+ movies.length);
 
 //STEP 4
+// window.console.log("#### Step 4 ####")
+
+// let movies = ["The Jungle", "The Big Lebowski", "11/22/63", "Employee of the Month", "Shawn of the Dead"];
+// window.console.log("Before delete function the array contained " +movies);
+
+// delete(movies[0]);
+
+// window.console.log("After the delete function the array contained " +movies);
 
 //STEP 5
+window.console.log("#### Step 5 ####")
+
+let movies = ["The Jungle", "The Big Lebowski", "11/22/63", "Employee of the Month", "Shawn of the Dead", "Super Troopers", "Beer Fest"];
+
+
+// window.console.log("Before delete function the array contained " +movies);
+
+// delete(movies[0]);
+
+// window.console.log("After the delete function the array contained " +movies);
 
 //STEP 6
 
